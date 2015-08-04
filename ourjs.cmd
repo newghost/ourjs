899d28fc35de0870eea4ce9a83b936776eba0fb7
Kris, Zhang Chunliang <kris.newghost@gmail.com>
2015-08-04 17:32:43 +0800

    update

--- ourjs.cmd
+++ ourjs.cmd
@@ -9,7 +9,7 @@ cd %~dp0
 
     node ./svr/ourjs.js config.magazine.js
 
-    ping -n 1 127.0.0.1 >nul
+    ping -n 5 127.0.0.1 >nul
     REM "Server stop working, waiting for restart...\r\n\r\n\r\n\r\n"
 
 goto :dowhile
